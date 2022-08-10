ac995456e2f078acd285f34953c8f02fc3636e51
Nithesh <nitheshurao@gmail.com>
2022-08-10 20:44:41 +0530

    E-commerce Final

--- backend/server.js
+++ backend/server.js
@@ -1,42 +1,60 @@
-import express from "express";
-import Color from "color";
-import dotenv from "dotenv";
-import connectDB from './config/db.js'
-import product from './data/products.js'
-// import product from "./models/productModel.js";
+import path from 'path'
+import express from 'express'
+import dotenv from 'dotenv'
+import colors from 'colors'
+import morgan from 'morgan'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import connectDB from './config/db.js'
 
-import ProductRouters from './routes/ProductRoutes.js'
-import UserRouter from './routes/UserRoutes.js'
-import cors from 'cors'
-
+import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 
 dotenv.config()
+
 connectDB()
 
-
-
-
 const app = express()
-app.use(cors)
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
 app.use(express.json())
 
-app.use('/api/product/', ProductRouters)
-app.use('/api/users/', UserRouter)
-// app.get('/api/product/', (req, res) => {
-//     res.send("API is running--")
+app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 
-//     res.json(product)
-//     // const
-// })
-app.get('/api/product/:id', (req, res) => {
-    // console.log("___", req)
-    // res.send("API is running--", req)
-    const pr = product.find(p => p._id === req.params.id)
-    // res.send("API is running--")
-    res.json(pr)
-    // const
-})
-app.use(notFound, errorHandler)
+app.get('/api/config/paypal', (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+)
+
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
+
+app.use(notFound)
+app.use(errorHandler)
+
 const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log("server is running--"))
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+)
